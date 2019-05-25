7c975e1acf9cb55bd5f7c14402246b711258fd11
diego <mirandadiego4@gmail.com>
2019-05-25 00:19:18 -0400

    version 1.1 de MergeSort , se introdujo un método que permita obtener información sobre la ejecución del algoritmo.

--- AlgoritmoMergeSort/src/algoritmomergesort/AlgoritmoMergeSort.java
+++ AlgoritmoMergeSort/src/algoritmomergesort/AlgoritmoMergeSort.java
@@ -2,22 +2,27 @@ package algoritmomergesort;
 
 /**
  * @author Diego Miranda 
- * @version 1.0
+ * @version 1.1
  */
 
 public class AlgoritmoMergeSort {
 
       /**
      * @author Diego Miranda
-     * @version 1.0
+     * @version 1.1
      * Método main para testear el algoritmo MergeSort.
      * @param args the command line arguments
      */
     public static void main(String[] args) {
            int []  A = { 4 , 3 , 2, 1 };
+           long tiempoInicio = 0, tiempoFinal = 0;
            MergeSort ordenamiento = new MergeSort(A);
+           tiempoInicio = System.currentTimeMillis();
            ordenamiento.mergeSort(A,0,3);
+           tiempoFinal = System.currentTimeMillis();
            ordenamiento.imprimirOrdenamiento();
+           ordenamiento.setTiempoEjecucion(tiempoFinal - tiempoInicio); 
+           ordenamiento.imprimirInformacion();
     }
     
 }
--- AlgoritmoMergeSort/src/algoritmomergesort/MergeSort.java
+++ AlgoritmoMergeSort/src/algoritmomergesort/MergeSort.java
@@ -3,7 +3,7 @@ package algoritmomergesort;
 
 /**
  * @author Diego Miranda
- * @version 1.0
+ * @version 1.1
  * clase MergeSort basada en CRLS que ejecuta el algoritmo MergeSort
  * está conformado por los métodos merge y mergeSort.
  */
@@ -11,89 +11,128 @@ package algoritmomergesort;
 public class MergeSort {
       
     private int[] A;
-     
+    private int[] B;
+    int numeroRotaciones;
+    int numeroComparaciones;
+    long tiempoEjecucion;
+           
+    ////////////////////////////////////////MÉTODO SET TIEMPO EJECUCIÓN////////////////////////////////////
+    /**@author Diego Miranda.
+     * @version 1.1
+     * método set para entregar el tiempo en milisegundos que tardo mergesort en ejecutarse.
+     * @param tiempoEjecucion : tiempo en milisegundos  
+     */
+    public void setTiempoEjecucion(long tiempoEjecucion) {
+        this.tiempoEjecucion = tiempoEjecucion;
+    }
+    
    //////////////////////////////////////////////CONSTRUCTOR///////////////////////////////////////////////////// 
     /**
      * @author Diego Miranda
-     * @version 1.0
+     * @version 1.1
      * Constructor para la clase MergeSort.
-     * @param A  : array unidimensional de números enteros
+     * asignamos a los atributos privados A y B el valor del array A
+     * e inicializamos los demas atributos por defecto.
+     * @param A  : array unidimensional de números enteros.
      */  
     public MergeSort( int[] A) {
          this.A = A;
-         
+         this.B =A;
+         numeroRotaciones = 0;
+         numeroComparaciones = 0;
+         tiempoEjecucion = 0;
     }
  
     /////////////////////////////////////////////MÉTODO MERGE/////////////////////////////////////////////////////
-    
     /**
      * @author Diego Miranda
-     * @version 1.0
+     * @version 1.1
      * Método merge para realizar la unión entre los elementos del array.
-     * @param A array del tipo especifico
+     * @param B array del tipo especifico
      * @param p indice inicial
      * @param q indice particion
      * @param r indice final
      */
-    
     //////////////////////////////////////////////////////////////////////////////////////////////////////
-    public void merge (int[] A  , int p , int q , int r) {
+    public void merge (int[] B  , int p , int q , int r) {
          int n1 = q - p + 1;
          int n2 = r - q;
          int[] R = new int[n2 + 1];
          int[] L = new int[n1 + 1];
          for (int i = 0 ; i < n1 ; i++) {
-            L[i] = A[p + i];
+            L[i] = B[p + i];
          }
          for (int j = 0 ; j < n2 ; j++) {
-            R[j] = A[q + j + 1]; 
+            R[j] = B[q + j + 1]; 
          }
          L[n1] = Integer.MAX_VALUE;
          R[n2] = Integer.MAX_VALUE;
          int i = 0;
          int j = 0;
          for ( int k = p ; k <= r  ; k ++) {
+                 numeroComparaciones++;
                  if (L[i] <= R[j]) {
-                     A[k] = L[i];
+                     B[k] = L[i];
                      i++;
                  }
                  else {
-                     A[k] = R[j];
+                     B[k] = R[j];
                      j++;
                  }
+                numeroRotaciones++;
          }
     }
    
     //////////////////////////////////////MÉTODO MERGESORT////////////////////////////////////////////////
     /**
      * @author Diego Miranda
-     *  @version 1.0
+     *  @version 1.1
      * Método para realizar la división del array en 2 mitades de manera recursiva y posterioremente su unión.
-     * @param A: array unidimensional de números enteros.
+     * @param B: array unidimensional de números enteros.
      * @param p: indice inicial del array o sub-array.
      * @param r: indice final del array o sub-array.
      */
-    public void mergeSort(int[] A  , int p , int r) {
+    public void mergeSort(int[] B  , int p , int r) {
         if ( p < r) {
            int q  = (int)( (p+r)/2 ); 
-           mergeSort(A , p , q);
-           mergeSort(A , q+1 , r);
-           merge(A , p , q , r );
-        
+           mergeSort(B , p , q);
+           mergeSort(B , q+1 , r);
+           merge(B , p , q , r );
         }
     }
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////MÉTODO IMPRIMIR ORDENAMIENTO ////////////////////////////
     /**
      * @author Diego Miranda
-     * @version 1.0
-     *  Imprimir los elementos del array A.  
+     * @version 1.1
+     *  Imprimir los elementos del array A y B.  
      */
     public void imprimirOrdenamiento () {
-        for (int i = 0 ; i < A.length ; i++ ) {
-              System.out.print(" " + A[i] + " " );
+        System.out.println("\nArray Inicial : ");
+        for (int i = 0 ; i < A.length ; i++) {
+             System.out.print(" " + A[i] + " ");
+        }
+        System.out.println("\nArray Ordenado : ");
+        for (int i = 0 ; i < B.length ; i++ ) {
+              System.out.print(" " + B[i] + " " );
         }
     }
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////MÉTODO IMPRIMIR INFORMACIÓN/////////////////////////////////
+    /**
+     *  @author Diego Miranda
+     *  @version 1.1
+     *  Imprimir la información acerca de la ejecución del algoritmo.
+     *  para que el tiempo de ejecución sea diferente de 0 debe llamarse primero
+     *  al método setTiempoEjecución.
+     */
+    public void imprimirInformacion () {
+       System.out.println("Información de la ejecución actual del algoritmo : ");
+       System.out.println("Número de rotaciones : " + numeroRotaciones);
+       System.out.println("Número de comparaciones : " + numeroComparaciones);
+       System.out.println("Tiempo de ejecucion en milisegundo : " + tiempoEjecucion );
+       System.out.println("Tiempo de ejecucion en segundos : " + tiempoEjecucion/1000);
+    }
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
 }
 
 
